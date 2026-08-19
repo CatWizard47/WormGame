45d6278ffda445944ec8a973a751872e57796d5c
Tomasz Teofilak <s227674@sggw.edu.pl>
2026-08-19 19:00:57 +0200

    more tests on legs

--- leg_movement.gd
+++ leg_movement.gd
@@ -1,5 +1,5 @@
 extends Sprite2D
-var leg_reach: int 
+var leg_reach: int = 10 
 var foot_placement: Vector2
 var foot: Sprite2D
 var arm: Sprite2D
@@ -14,16 +14,22 @@ func setup(reach: int, time: float) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	parent_velocity = get_parent().velocity
-	print(parent_velocity)
 	if(!step_happening_flag):
-		foot.global_position -= parent_velocity
-		foot.position.x = clampf(foot.position.x, -100,100)
-		foot.position.y = clampf(foot.position.y, -100,100) 
-		pass
+		#foot.global_position = foot_placement
+		#if inverted_orientation_flag:
+		#	pass
+		#else:
+		#	pass
+		foot.global_position = foot_placement
 	else:
-		step_happening_flag = false
-		pass
-
+		if inverted_orientation_flag:
+			foot_placement = global_position + parent_velocity/2.0 + parent_velocity.orthogonal() / 2.0
+		else:
+			foot_placement = global_position + parent_velocity/2.0 - parent_velocity.orthogonal() / 2.0 
+		foot.global_position = foot_placement
+		if foot_placement.length()>= leg_reach:
+			step_happening_flag = false
+		print(foot_placement)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -32,8 +38,13 @@ func _ready() -> void:
 	foot = get_node("leg_arm/leg_foot")
 	if(rotation < 0):
 		inverted_orientation_flag = true #left side of the hull
+		arm.position.x += leg_reach / 2.0
+		foot.position.x += leg_reach
 	else:
 		inverted_orientation_flag = false 
+		arm.position.x += leg_reach / 2.0
+		foot.position.x += leg_reach
+	foot_placement = global_position 
 	pass 
 
 
