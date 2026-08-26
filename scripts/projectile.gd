2c15a743d8852392d101a7f34562721a5c746228
Tomasz Teofilak <s227674@sggw.edu.pl>
2026-08-26 17:47:14 +0200

    raycasting my beloved, fixed collision issues with bullets

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -17,14 +17,12 @@ static func setup(new_position: Vector2, new_rotation: float, Projectile:Project
 func _ready() -> void:
 	position = start_position
 	rotation = start_rotation
-	get_node("Sprite2D").rotation = start_rotation
-	get_node("ProjectileCollision")
 	if projectile_stats.projectile_speed == 0:
 		_hitscan_fire()
 	else:
 		velocity = Vector2.from_angle(start_rotation).normalized() * projectile_stats.projectile_speed
 		var new_collision_shape = RectangleShape2D.new() 
-		new_collision_shape.set_size(Vector2(projectile_stats.projectile_speed * 10, 5))
+		new_collision_shape.set_size(Vector2(10, 5))
 		#new_collision_shape.set_radius(5.1)
 		#print(velocity)
 		self.collision_mask = 12
@@ -34,18 +32,21 @@ func _ready() -> void:
 	#later tho, now base implement
 
 
+func is_valid_target(id: int) -> bool:
+	return (!instance_from_id(id).has_method("_on_impact") and !instance_from_id(id).is_queued_for_deletion() and instance_from_id(id).is_class("RigidBody2D"))
+
 func _physics_process(delta: float) -> void: 
 	#print(delta)
-	move_and_collide(velocity * delta * 100,false,0.1)
+	move_and_collide(velocity * delta,false,0.1)
 	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(position, velocity * delta * 100)
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + velocity.normalized() * 10 )
 	query.exclude = [self.get_rid()]
 	var result = space_state.intersect_ray(query)
-	print(instance_from_id(result.collider_id).has_method("_on_impact"))
-	if collision_flag and !instance_from_id(result.collider_id).has_method("_on_impact") and !instance_from_id(result.collider_id).is_queued_for_deletion():
-		_deal_damage(instance_from_id(result.collider_id))
-	#if !instance_from_id(result.collider_id).get_class() == "Projectile":
-	#	queue_free()
+	if !result.is_empty():
+		if collision_flag and is_valid_target(result.collider_id):
+			_deal_damage(instance_from_id(result.collider_id))
+		elif(collision_flag and instance_from_id(result.collider_id).is_class("StaticBody2D")):
+			queue_free()
 	
 
 
