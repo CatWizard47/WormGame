88c6db3497f686ed1b5950949c81ff52f5bb99f5
Tomasz Teofilak <s227674@sggw.edu.pl>
2026-09-07 18:44:59 +0200

    added a simple validation function to PFM

--- scripts/pathfinding_manager.gd
+++ scripts/pathfinding_manager.gd
@@ -1,14 +1,22 @@
-extends Node2D	#needs to be a 2D node, due to world2D usage
+class_name PathfindingManager extends Node2D	#needs to be a 2D node, due to world2D usage
 # Called when the node enters the scene tree for the first time.
 #@export var node_border_lenght: int
 @export var node_size: int #dist from the center so a node with size 10 is 20x20 square 
 @export var maximum_sector_size: int #in nodes width from the center so 2* this for absolute width | height
 var shape_rid: RID
 var available_pathfinding_sectors: Array
-
+var space_state: PhysicsDirectSpaceState2D
 
 
 func _ready() -> void:
+	space_state = get_world_2d().direct_space_state
+	shape_rid = PhysicsServer2D.rectangle_shape_create()
+	PhysicsServer2D.shape_set_data(shape_rid,Vector2(node_size,node_size))
+	#var params: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
+	#var body_position: Vector2 = (PhysicsServer2D.body_get_state(body_rid,PhysicsServer2D.BODY_STATE_TRANSFORM).get_origin())
+	#params.motion = body_position
+	#params.shape_rid = explosion_shape_rid# Execute physics queries here...# Release the shape when done with physics queries.
+	#var explosion_result = space_state.intersect_shape(params,32)
 	pass # Replace with function body.
 
 
@@ -23,6 +31,16 @@ func generate_pathfinding_sector(starting_position:Vector2i)->void: #->Pathfindi
 func generate_pathfinding_node(starting_position:Vector2i)->void: #->PathfindingNode
 	pass
 
+
+	#needs to check whether a given node placement is accesible at all:
+func _check_collisions(shape_intersect:Array[Dictionary])->bool:
+	for result:Dictionary in shape_intersect:
+		if !result.is_empty() and instance_from_id(result.get("collider_id")) != null:
+			if instance_from_id(result.get("collider_id")).is_class("StaticBody2D"):
+				return false #provided I won't add any more stuff that's meant to block things, this should be fine 
+	return true	
+	#above thing might even need to be simplyfied since we will only be checking collision layer 4 specifically
+	#TODO check: /|\
 
 												#if z-levels are to be implemented, i have to dynamically change col_layer of obj
 
